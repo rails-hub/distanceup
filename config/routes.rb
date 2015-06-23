b0cc9316de254ce30002ae7b5a2f384f48f58bbe
todd <todd@todd.(none)>
2015-06-23 15:17:15 +0500

    make register GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  match 'api/v1/register' => 'api/v1#register_user', via: [:post]
+  match 'api/v1/register' => 'api/v1#register_user', via: [:get]
   match 'api/v1/find_nearby' => 'api/v1#find_nearby', via: [:get]
 
   root to: "home#index"
